9e4f3f13375431fc2f546b37d762ebaac60ea2ed
Darren Russell <darren.russell@erudus.com>
2023-03-20 19:04:29 +0000

    build and style intro section

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,11 +1,53 @@
-import React from 'react';
+import React, { useState } from "react";
+import { BsFillPlayFill, BsFillPauseFill } from "react-icons/bs";
+import { meal } from "../../constants";
+import "./Intro.css";
 
-import './Intro.css';
+const Intro = () => {
+	const [playVideo, setPlayVideo] = useState(false);
+	const [videoClass, setVideoClass] = useState(
+		"app__video-overlay flex__center"
+	);
+	const vidRef = React.useRef();
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+	const handleVideo = () => {
+		setPlayVideo((prevPlayVideo) => !prevPlayVideo);
+
+		if (playVideo) {
+			vidRef.current.pause();
+		} else {
+			vidRef.current.play();
+		}
+	};
+
+	return (
+		<div
+			className="app__video"
+			onMouseEnter={() => setVideoClass("app__video-overlay flex__center")}
+			onMouseLeave={() => setVideoClass("")}
+		>
+			<video
+				src={meal}
+				type="video/mp4"
+				ref={vidRef}
+				loop
+				controls={false}
+				muted
+			/>
+			<div className={videoClass}>
+				<div
+					className="app__video-overlay_circle flex__center"
+					onClick={handleVideo}
+				>
+					{playVideo ? (
+						<BsFillPauseFill color="#fff" fontSize={30} />
+					) : (
+						<BsFillPlayFill color="#fff" fontSize={30} />
+					)}
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default Intro;
